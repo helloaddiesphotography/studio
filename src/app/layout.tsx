3d4cd456501c03048bec535fba1e4ece6ddd3ba6
Adilene García <helloaddiesphotography@gmail.com>
2025-10-23 05:02:55 +0000

    Try fixing this error: `Console Error: In HTML, <script> cannot be a chi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,17 +23,17 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        <body className="font-body antialiased">
+      <body className="font-body antialiased">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
           <Toaster />
-        </body>
-      </ThemeProvider>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
